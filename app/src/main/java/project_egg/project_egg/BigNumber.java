f64f826941377803affd0e1f77aec86940bbd6f7
Luke <luke.iannucci@gmail.com>
2016-12-19 15:34:17 -0700

    implementing bignumber

--- app/src/main/java/project_egg/project_egg/BigNumber.java
+++ app/src/main/java/project_egg/project_egg/BigNumber.java
@@ -5,9 +5,17 @@ package project_egg.project_egg;
  */
 
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BigNumber {
     int count;
+
+    List<Integer> bigNumber = new ArrayList();
+
+    public BigNumber(int num) {
+        bigNumber.add(0, num);
+    }
 
     public BigNumber() {
         count = 0;
