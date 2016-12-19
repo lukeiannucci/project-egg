ac91ff4ce2632178e51647360a5c66ce21615f9a
jmatt <jmattingley23@gmail.com>
2016-12-19 15:02:23 -0700

    egg textview added

--- app/src/main/java/project_egg/project_egg/MainActivity.java
+++ app/src/main/java/project_egg/project_egg/MainActivity.java
@@ -3,6 +3,7 @@ package project_egg.project_egg;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Luke on 12/19/2016.
@@ -14,11 +15,18 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+        update();
     }
 
     protected void onButtonClick(View egg) {
         if(egg.getId() == R.id.golden_egg) {
             egg_count++;
+            update();
         }
+    }
+
+    public void update() {
+        TextView eggCountDisplay = (TextView) findViewById(R.id.eggCountTextView);
+            eggCountDisplay.setText(String.valueOf(egg_count));
     }
 }
