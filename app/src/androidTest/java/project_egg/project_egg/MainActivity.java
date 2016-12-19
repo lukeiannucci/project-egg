57c6880443b0f00fb13b29ab6237d6ae21b0063c
Luke <luke.iannucci@gmail.com>
2016-12-19 14:26:42 -0700

    GOLDEN EGG!

--- app/src/androidTest/java/project_egg/project_egg/MainActivity.java
+++ app/src/androidTest/java/project_egg/project_egg/MainActivity.java
@@ -1,9 +1,16 @@
 package project_egg.project_egg;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Luke on 12/19/2016.
  */
 
-public class MainActivity {
-    //test push
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
+    }
 }
