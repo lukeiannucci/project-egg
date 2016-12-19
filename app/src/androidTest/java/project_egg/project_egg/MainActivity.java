40f7627eae837679fc9ba51b7e3dd7d601dfb069
Luke <luke.iannucci@gmail.com>
2016-12-19 14:37:17 -0700

    running version

--- app/src/androidTest/java/project_egg/project_egg/MainActivity.java
+++ app/src/androidTest/java/project_egg/project_egg/MainActivity.java
@@ -2,15 +2,23 @@ package project_egg.project_egg;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Luke on 12/19/2016.
  */
 
 public class MainActivity extends AppCompatActivity{
+    int egg_count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
+    }
+
+    protected void onButtonClick(View egg) {
+        if(egg.getId() == R.id.golden_egg) {
+            egg_count++;
+        }
     }
 }
