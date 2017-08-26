3854e995c9531639eb3b60df87c0057b8ebf9323
Luke Iannucci <luke.iannucci@outlook.com>
2017-08-25 22:27:44 -0600

    javafx masters we out here

--- src/xyz/jmatt/login/LoginController.java
+++ src/xyz/jmatt/login/LoginController.java
@@ -2,6 +2,7 @@ package xyz.jmatt.login;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseButton;
@@ -11,6 +12,9 @@ import javafx.stage.Stage;
 
 
 public class LoginController {
+    @FXML
+    private BorderPane LoginPane;
+
     @FXML
     private void OnMouseEntered(MouseEvent event){
         Button myButton = (Button)event.getSource();
@@ -27,12 +31,13 @@ public class LoginController {
         if(event.getButton() == MouseButton.PRIMARY){
                try {
                    BorderPane CreateAccountPane = FXMLLoader.load(getClass().getResource("/xyz/jmatt/createaccount/CreateAccount.fxml"));
-                   Stage createAccountWindow = new Stage();
-                   createAccountWindow.setScene(new Scene(CreateAccountPane, 600, 400));
-                   createAccountWindow.show();
-                   createAccountWindow.setMinWidth(createAccountWindow.getWidth());
-                   createAccountWindow.setMinHeight(createAccountWindow.getHeight());
-                   //((Node) (event.getSource())).getScene().getWindow().;
+                   //Stage createAccountWindow = new Stage();
+                   //createAccountWindow.setScene(new Scene(CreateAccountPane, 600, 400));
+                   //createAccountWindow.show();
+                   //createAccountWindow.setMinWidth(createAccountWindow.getWidth());
+                   //createAccountWindow.setMinHeight(createAccountWindow.getHeight());
+                   LoginPane.getChildren().setAll(CreateAccountPane);
+                   //((BorderPane)(event.getSource())).;
                }
                catch (Exception e){
                    e.printStackTrace();
