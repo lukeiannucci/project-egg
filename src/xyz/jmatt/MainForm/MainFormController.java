6d7a552e33e3c66a78384eaebc301f4bd3d4775f
Luke Iannucci <luke.i@taniustech.com>
2017-08-29 16:02:29 -0600

    tableheader moving set to false

--- src/xyz/jmatt/MainForm/MainFormController.java
+++ src/xyz/jmatt/MainForm/MainFormController.java
@@ -1,17 +1,23 @@
 package xyz.jmatt.MainForm;
 
+import com.sun.javafx.scene.control.skin.TableHeaderRow;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import xyz.jmatt.models.TransactionModel;
+import xyz.jmatt.services.TransactionService;
 
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.*;
+
+import static java.lang.invoke.MethodHandles.lookup;
 
 public class MainFormController extends MenuItem implements Initializable {
     @FXML
@@ -45,7 +51,9 @@ public class MainFormController extends MenuItem implements Initializable {
     @FXML
     private void AddTransactionModel()
     {
-        data.add(new TransactionModel(TransIn.getText(),CategoryIn.getText(),new BigDecimal(AmountIn.getText()), Long.valueOf(DateIn.getText())));
+        TransactionModel trans = new TransactionModel(TransIn.getText(),CategoryIn.getText(),new BigDecimal(AmountIn.getText()), Long.valueOf(DateIn.getText()));
+        new TransactionService().addTransaction(trans);
+        data.add(trans);
         TransIn.setText("");
         CategoryIn.setText("");
         AmountIn.setText("");
@@ -54,13 +62,8 @@ public class MainFormController extends MenuItem implements Initializable {
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        List<TransactionModel> myList = new ArrayList<TransactionModel>();
-        myList.add(new TransactionModel("McDicks", "Food", new BigDecimal(1), 1));
-        myList.add(new TransactionModel("Gym", "Sports", new BigDecimal(2), 2));
-        myList.add(new TransactionModel("McDicks", "Food", new BigDecimal(3), 3));
-
         data = FXCollections.observableArrayList(
-                myList
+                new TransactionService().getAllTransactions()
         );
         Name.setCellValueFactory(
                 new PropertyValueFactory<TransactionModel, String>("Name")
@@ -77,13 +80,11 @@ public class MainFormController extends MenuItem implements Initializable {
         Total.setCellValueFactory(
                 new PropertyValueFactory<TransactionModel, String>("Total")
         );
-        TransactionTable.setItems(data);
-        //myList.add("hello");
 
-        //final Menu menu1 = new Menu("File");
-        //menu1.show();
-        //MenuBar menuBar = new MenuBar();
-        //menuBar.getMenus().add(menu1);
-        //MainForm.getChildren().add(menuBar);
+        TransactionTable.skinProperty().addListener((observable, oldValue, newValue) -> {
+            final TableHeaderRow header = (TableHeaderRow)TransactionTable.lookup("TableHeaderRow");
+            header.reorderingProperty().addListener((observable1, oldValue1, newValue1) -> {header.setReordering(false);});
+        });
+        TransactionTable.setItems(data);
     }
 }
