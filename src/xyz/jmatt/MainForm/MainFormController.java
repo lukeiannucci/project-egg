b31870099215b81e8b2f6c54e6e0015114a46ab2
Luke Iannucci <luke.i@taniustech.com>
2017-08-29 14:02:07 -0600

    hello boys

--- src/xyz/jmatt/MainForm/MainFormController.java
+++ src/xyz/jmatt/MainForm/MainFormController.java
@@ -1,13 +1,16 @@
 package xyz.jmatt.MainForm;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainFormController extends MenuItem implements Initializable {
@@ -15,8 +18,13 @@ public class MainFormController extends MenuItem implements Initializable {
     private BorderPane MainForm;
     @FXML
     private MenuBar MainMenuBar;
+    @FXML
+    private TableView TransactionTable;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        //myList.add("hello");
+        
         //final Menu menu1 = new Menu("File");
         //menu1.show();
         //MenuBar menuBar = new MenuBar();
